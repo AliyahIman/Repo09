5acc91b8824a8c0a2c53ac22284e2ea0025ed597
AliyahIman <90403246+AliyahIman@users.noreply.github.com>
2021-09-22 02:50:36 +0200

    Gorillaz - Rhinestone Eyes

--- ex13/src/index.js
+++ ex13/src/index.js
@@ -1,1 +1,20 @@
+// Write code below this line
 
+function inverseWhile(){
+
+    var fiveNumbers = "";
+
+    var i=5;
+
+    while (i >=0) {
+        fiveNumbers +=i;
+        fiveNumbers +=","
+        i--;
+    }
+    fiveNumbers = fiveNumbers.substring(0,11);
+    return fiveNumbers;
+}
+// Write code above this line
+
+console.log(inverseWhile()); 
+module.exports = inverseWhile;
