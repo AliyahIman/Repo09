58024aca1b36308f104aa93c94f50a53942d583d
AliyahIman <90403246+AliyahIman@users.noreply.github.com>
2021-09-22 02:56:42 +0200

    Unlike Pluto - Unless it happens to you

--- ex14/src/index.js
+++ ex14/src/index.js
@@ -1,1 +1,33 @@
+// Only change code below this line
 
+function myForLoop1(){
+
+    evenNumbers = "";
+
+    for (var i=0; i<5; i++) {
+        evenNumbers += (2 * i);
+        evenNumbers += ",";
+    }
+
+    evenNumbers = evenNumbers.substring(0,9); //Removing last coma
+    return evenNumbers;
+ }
+    
+ function myForLoop2(){
+    evenInverseNumbers = "";
+
+    for (var i=0; i<5; i++) {
+        evenInverseNumbers += 8 - (2 * i);
+        evenInverseNumbers += ",";
+    }
+    evenInverseNumbers = evenInverseNumbers.substring(0,9); //Removing last coma
+    return evenInverseNumbers;
+ }
+ console.log(myForLoop1()); 
+ console.log(myForLoop2())
+// Only change code above this line
+
+module.exports = {
+    myForLoop1,
+    myForLoop2
+}
