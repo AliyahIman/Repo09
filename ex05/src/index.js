c25b2c49b4c0c767e1fa3e02a2a159d6f34bf4c2
AliyahIman <90403246+AliyahIman@users.noreply.github.com>
2021-09-22 02:12:56 +0200

    Nothing but thieves - Honey whiskey

--- ex05/src/index.js
+++ ex05/src/index.js
@@ -1,1 +1,15 @@
+// Only change code below this line
+function compareStrictValues(m){
+    if (55 != m)
+        {return "Not equal";}
+    else
+        {return "Equal";}
+}
+console.log(compareStrictValues("55"));
+console.log(compareStrictValues(55));
+console.log(compareStrictValues("21"));
+console.log(compareStrictValues(12));
+console.log(compareStrictValues("Doe"));
 
+// Only change code above this line
+module.exports = compareStrictValues;
