a9926eb6f371d963647e93214fe528653558d77d
AliyahIman <90403246+AliyahIman@users.noreply.github.com>
2021-09-22 02:02:44 +0200

    Phantogram - Black out Days

--- ex02/src/index.js
+++ ex02/src/index.js
@@ -1,1 +1,13 @@
+// Only change code below this line 
+function myFunction(myParametar){
 
+    if (myParametar)
+        {return "The perameter is true!";}
+    else
+        {return "The perameter is false!";}
+}
+console.log(myFunction(true));
+console.log(myFunction(false));
+
+// Only change code above this line
+module.exports = myFunction;
