a438d9d93dc653a4fb618711dafe63cab373d43e
AliyahIman <90403246+AliyahIman@users.noreply.github.com>
2021-09-22 03:06:26 +0200

    Through Fire - Listen to you heart (roxette cover)

--- ex15/src/index.js
+++ ex15/src/index.js
@@ -1,1 +1,19 @@
+ // Only change code below this line
 
+ function myDoWhile(){
+
+    var myNumbers ="";
+
+    var i=0;
+    do {
+        myNumbers +=i;  
+        myNumbers+= ",";
+        i++;
+    } while (i<10);
+    myNumbers=myNumbers.substring(0,19);
+    return myNumbers;
+ }
+ console.log(myDoWhile());
+// Only change code above this line
+
+module.exports = myDoWhile;
