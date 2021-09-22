ff81b3d6a256d952c884456345eb1b3f863ed9fc
AliyahIman <90403246+AliyahIman@users.noreply.github.com>
2021-09-22 02:09:52 +0200

    Savage Garden - To the moon and back

--- ex04/src/index.js
+++ ex04/src/index.js
@@ -1,1 +1,12 @@
+// Only change code below this line
+function compareDifferentValues(m,n){
 
+    if (n === m)
+        {return "Equal";}
+    else
+        {return "Not equal";}
+}
+console.log(compareDifferentValues(8,"8"));
+console.log(compareDifferentValues(8,8));
+// Only change code above this line
+module.exports = compareDifferentValues;
