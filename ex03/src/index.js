5494668d4b0478cfe4299e189d4b407f8049bf51
AliyahIman <90403246+AliyahIman@users.noreply.github.com>
2021-09-22 02:05:27 +0200

    Nelly Furtado - Maneater

--- ex03/src/index.js
+++ ex03/src/index.js
@@ -1,1 +1,12 @@
+// Only change code below this line
+function myEqualFunction(num){
+    if (num === 23)
+        {return "Equal";}
+    else
+        {return "Not equal";}
+}
+console.log(myEqualFunction(23));
+console.log(myEqualFunction("23"));
+// Only change code above this line
 
+module.exports = myEqualFunction;
